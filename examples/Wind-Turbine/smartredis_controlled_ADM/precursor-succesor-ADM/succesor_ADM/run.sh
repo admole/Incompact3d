865d1e36773de18f61d1f5da01421dc80459c75c
admole <a.mole@imperial.ac.uk>
2024-06-21 17:30:26 +0100

    Dont let LD_LIBRARY_PATH escape!

--- examples/Wind-Turbine/smartredis_controlled_ADM/precursor-succesor-ADM/succesor_ADM/run.sh
+++ examples/Wind-Turbine/smartredis_controlled_ADM/precursor-succesor-ADM/succesor_ADM/run.sh
@@ -1,11 +1,15 @@
-# Python
+#!/bin/sh
+
+# Python controller
 export SR_DB_TYPE="Standalone"
 export SSDB=127.0.0.1:6781
 python controller.py&
 
 sleep 20
 
-LD_LIBRARY_PATH=/home/amole/Code/Incompact3d/build/smartredis-build/smartredis/install/lib:$LD_LIBRARY_PATH \
+# Xcompact3d run
+LD_LIBRARY_PATH=/home/amole/Documents/Incompact3d/build/smartredis-build/smartredis/install/lib:$LD_LIBRARY_PATH \
+export LD_LIBRARY_PATH
 
 # mpirun -np 8 ../../../../build/bin/xcompact3d > log.x3d
-../../../../../build/bin/xcompact3d > log.x3d
+exec ../../../../../build/bin/xcompact3d > log.x3d
